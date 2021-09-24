cf54379075b79a8889866ba95e02f415dce85935
aaron-KO <56333396+aaron-KO@users.noreply.github.com>
2021-09-24 15:06:45 -0400

    splash screen connect main

--- app/src/main/java/com/example/propertymanagementapp/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/propertymanagementapp/activity/RegisterActivity.kt
@@ -3,10 +3,6 @@ package com.example.propertymanagementapp.activity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.propertymanagementapp.R
 import com.example.propertymanagementapp.databinding.ActivityRegisterBinding
 
