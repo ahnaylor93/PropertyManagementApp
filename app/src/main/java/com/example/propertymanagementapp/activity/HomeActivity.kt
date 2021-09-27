9d95946c7e962758bb0ca84df0fac56d9d561b1e
aaron-KO <56333396+aaron-KO@users.noreply.github.com>
2021-09-27 09:53:56 -0400

    add progress bar

--- app/src/main/java/com/example/propertymanagementapp/activity/HomeActivity.kt
+++ app/src/main/java/com/example/propertymanagementapp/activity/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.propertymanagementapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.propertymanagementapp.databinding.ActivityHomeBinding
@@ -13,5 +14,15 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupEvents()
+    }
+    fun setupEvents(){
+        binding.btnAlerts.setOnClickListener(){
+
+        }
+        binding.btnProperties.setOnClickListener(){
+            startActivity(Intent(baseContext,PropertyScreenActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/propertymanagementapp/activity/PropertyScreenActivity.kt
+++ app/src/main/java/com/example/propertymanagementapp/activity/PropertyScreenActivity.kt
@@ -1,4 +1,12 @@
 package com.example.propertymanagementapp.activity
 
-class PropertyScreenActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.propertymanagementapp.R
+
+class PropertyScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_property_screen)
+    }
 }
