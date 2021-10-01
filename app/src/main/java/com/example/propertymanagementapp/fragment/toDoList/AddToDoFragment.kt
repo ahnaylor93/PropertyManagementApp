d2ab2d3f84873a0796468a1b11c2c21c650ad825
aaron-KO <56333396+aaron-KO@users.noreply.github.com>
2021-10-01 01:31:33 -0400

    todolist fragment and date bug fiexed

--- app/src/main/java/com/example/propertymanagementapp/fragment/toDoList/AddToDoFragment.kt
+++ app/src/main/java/com/example/propertymanagementapp/fragment/toDoList/AddToDoFragment.kt
@@ -11,15 +11,20 @@ import com.example.propertymanagementapp.sql.AppDatabase
 import com.example.propertymanagementapp.sql.Todo
 import com.example.propertymanagementapp.viewmodel.TodoViewModel
 import com.example.propertymanagementapp.viewmodel.TodoViewModelFactory
+import android.R
+import android.graphics.Color
+
 
 class AddToDoFragment :Fragment(){
     lateinit var binding: FragmentAddTodoBinding
     lateinit var viewModel: TodoViewModel
+    var dateStr:String ="9/ 30/ 2021"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         binding = FragmentAddTodoBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
@@ -28,10 +33,16 @@ class AddToDoFragment :Fragment(){
         val tenantDao = AppDatabase.getInstance(requireActivity()).todoDao
         val factory = TodoViewModelFactory(tenantDao)
         viewModel = ViewModelProvider(requireActivity(), factory).get(TodoViewModel::class.java)
+        binding.cvDueDay.setOnDateChangeListener(){
+                view, year, month, dayOfMonth ->
+
+            dateStr =  "${(month + 1)}/ $dayOfMonth/ $year"
+
+        }
         binding.btnAdd.setOnClickListener(){
             val title = binding.etTitle.text.toString()
             val desc = binding.etDesc.text.toString()
-            val due = binding.cvDueDay.dateTextAppearance.toString()
+            val due = dateStr
             val todo = Todo(0,title,desc,due)
 
             viewModel.addTodo(todo)
